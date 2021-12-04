f2b5b60096c0925fc44df6b4dfeb88819434ed07
rafaelochoamello <rafaelochoamello@gmail.com>
2021-12-04 12:09:04 -0300

    ♻️ Base project refactor

---
+++ src/components/Navbar/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import './Navbar.css'
+
+const Navbar = () => {
+    return (
+        <>
+            <div className="Navbar">
+                <p>adlaskdçlask</p>
+            </div>
+        </>
+    )
+}
+
+export default Navbar;
